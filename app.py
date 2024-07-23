cd5b18a44c600c4f44dc859062a5c6ea52c3cd5b
Corporativo Daniel <daniel@corporativo.com>
2024-07-23 12:09:41 -0300

    adding archives

--- app.py
+++ app.py
@@ -58,6 +58,22 @@ def update_task(id):
 
      return jsonify({"menssage": "Tarefa atualizada com sucesso"})
 
+
+@app.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+     task = None
+
+     for t in tasks:
+          if t.id == id:
+               task = t
+
+     if not task:
+          return jsonify({"message": "Não foi possivel encontrar atividade"}), 404
+     
+     tasks.remove(task)
+
+     return jsonify({"menssage": "Tarefa deletada com sucesso"})
+
 if __name__ == "__main__":
      app.run(debug=True)
 

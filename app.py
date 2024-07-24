3c976b686abd741f6abb114786576df36ed51673
Corporativo Daniel <daniel@corporativo.com>
2024-07-24 09:12:10 -0300

    Adding tests $ Updating create endpoint

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def create_task():
      new_task = task(id=task_id_control,title=data['title'], description=data.get("description", ""))
      task_id_control += 1
      tasks.append(new_task)
-     return jsonify({"menssage": "Nova tarefa criada com sucesso"})
+     return jsonify({"message": "Nova tarefa criada com sucesso", "id": new_task.id})
 
 @app.route('/tasks', methods=['GET'])
 def get_task():
@@ -56,7 +56,7 @@ def update_task(id):
 
      print(task)
 
-     return jsonify({"menssage": "Tarefa atualizada com sucesso"})
+     return jsonify({"message": "Tarefa atualizada com sucesso"})
 
 
 @app.route('/tasks/<int:id>', methods=['DELETE'])
